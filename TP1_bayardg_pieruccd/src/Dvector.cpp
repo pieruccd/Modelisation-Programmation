5236e9df7c0ae74b6bb04cad73c161872c1eba90
Guillaume Bayard <guillaume.bayard@ensimag.grenoble-inp.fr>
2017-02-06 01:43:24 +0100

    maj

--- TP1_bayardg_pieruccd/src/Dvector.cpp
+++ TP1_bayardg_pieruccd/src/Dvector.cpp
@@ -1,14 +1,10 @@
-class Dvector
-{
-	public :
-		int dim;
-		Dvector (int d);
-	private :
-		double *p;
+#include "Dvector.h"
+#include <stdlib.h>
 
-		~Dvector();
-
-void display (std::ostream& str)
+void Dvector::display (std::ostream& str){
+	for (int i=0; i<this->dim; i++){
+		str<<p[i]<<"\n";
+	}
 }
 
 Dvector::~Dvector()
@@ -16,16 +12,52 @@ Dvector::~Dvector()
 	delete [] p;
 }
 
-Dvector::Devector()
+Dvector::Dvector(int d)
+{
+	dim = 0;
+	p=new double[dim];
+	for (int i=0; i<dim; i++){
+		p[i] = 0;
+	}
+}
+
+Dvector::Dvector()
 {
 	dim = 0;
 	p= new double[0];
 	p[0] = 0;
 }
-Dvector::Devector (int d, double value)
+
+Dvector::Dvector (int d, double value)
 {
 	dim = d;
 	p = new double[dim];
 	for (int i=0; i<dim; i++)
 		p[i] = value;
 }
+
+int Dvector::size(){
+	return this->dim;
+}
+
+void Dvector::fillRandomly(){
+	for (int i=0;i<this->dim;i++){
+		p[i]=((double) rand() / (RAND_MAX));
+	}
+}
+
+Dvector::Dvector(const Dvector & v)
+{
+	dim = v.dim;
+	p=new double[dim];
+	for (int i=0;i<dim;i++){
+		p[i]=v.p[i];
+	}
+}
+
+
+
+
+
+
+
